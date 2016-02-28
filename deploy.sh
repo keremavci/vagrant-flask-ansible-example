de57ae8c5c86303f8bf52faa894b473f6d528842
Kerem Avcı <avci.kerem@gmail.com>
2016-02-28 20:02:16 +0200

    Add tag and change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
- ansible-playbook ansible/deploy.yml -i ansible/hosts -u vagrant --private-key=.vagrant/machines/default/virtualbox/private_key
+# ansible-playbook ansible/deploy.yml -i ansible/hosts -u vagrant --private-key=.vagrant/machines/default/virtualbox/private_key
+ansible-playbook ansible/webserver.yml --tags "deploy" -i ansible/hosts -u vagrant --private-key=.vagrant/machines/default/virtualbox/private_key 
